e3fc3cf31933be437b6405e4739acacbafcbdaff
Dawsonkw <dawsonkw@gmail.com>
2023-03-28 08:13:27 -0600

    initial commit, testing SCSS

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import './styles.scss'
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <button className='button'>
+        Press Me Please!!!!
+      </button>
+    </div>
+  )
+}
+
+export default App
